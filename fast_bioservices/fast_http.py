b132df66571b0235214f5aa0872db06a53f57ab5
Josh Loecker <joshloecker@icloud.com>
2024-02-26 15:21:54 -0600

    Added union of dictionary or list[dictionary]

--- fast_bioservices/fast_http.py
+++ fast_bioservices/fast_http.py
@@ -1,5 +1,5 @@
 import urllib.parse
-from typing import Optional, Union
+from typing import List, Optional, Union
 
 import modguard
 import requests
@@ -14,23 +14,23 @@ class HTTP:
     _cache: Optional[bool] = None
     _session: requests_cache.CachedSession = None
     warned: bool = False
-
+    
     def __init__(self, cache: bool) -> None:
         self._use_cache: bool = cache
-
+        
         if HTTP._session is None:
             HTTP._session = requests_cache.CachedSession(cache_name=cache_name)
             if not self._use_cache:
                 HTTP._session.settings.disabled = True
         self._session = HTTP._session
-
+    
     def _get(
         self, url, _internal_check: bool = None
     ) -> Union[requests.Response, requests.exceptions.ConnectionError]:
         parts = urllib.parse.urlparse(url)
         url = requests.utils.requote_uri(url)
         logger.debug(f"Getting {url}")
-
+        
         try:
             if _internal_check:
                 self._session.settings.disabled = True
@@ -44,17 +44,17 @@ class HTTP:
                 self.warned = True
             return e
         return response
-
+    
     def _get_internal_json(self, url) -> dict:
         return self._get(url, _internal_check=True).json()
-
+    
     def _get_internal_text(self, url) -> str:
         return self._get(url, _internal_check=True).text
-
+    
     @modguard.public(allowlist=["fast_bioservices.biodbnet"])
-    def get_json(self, url) -> dict:
+    def get_json(self, url) -> Union[dict, List[dict]]:
         return self._get(url, _internal_check=False).json()
-
+    
     @modguard.public(allowlist=["fast_bioservices.biodbnet"])
     def get_text(self, url) -> str:
         return self._get(url, _internal_check=False).text
