fe703763d29a0c0887ce4ecd9aae09553a96814e
Josh Loecker <joshloecker@icloud.com>
2024-02-26 14:12:36 -0600

    Added additional logging settings

--- fast_bioservices/log.py
+++ fast_bioservices/log.py
@@ -1,3 +1,11 @@
 import logging
 
+import fast_bioservices.settings
+
 logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename=fast_bioservices.settings.log_filepath,
+)
