cf49e7a2ce15472480ec53f5c98a11d4d81db361
Josh Loecker <joshloecker@gmail.com>
2024-06-24 14:36:38 -0500

    feat: Add fuzzy search for species name validation

--- src/fast_bioservices/ensembl/ensembl.py
+++ src/fast_bioservices/ensembl/ensembl.py
@@ -1,5 +1,25 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from fast_bioservices._base import BaseModel
 from fast_bioservices._fast_http import FastHTTP
-from fast_bioservices.base import BaseModel
+from fast_bioservices._utils import fuzzy_search
+
+
+@dataclass
+class Species:
+    strain_collection: str
+    aliases: list[str]
+    name: str
+    taxon_id: int
+    assembly: str
+    common_name: str
+    groups: list[str]
+    accession: str
+    release: int
+    division: str
+    strain: str
+    display_name: str
 
 
 class Ensembl(BaseModel, FastHTTP):
@@ -10,18 +30,59 @@ class Ensembl(BaseModel, FastHTTP):
         cache: bool = True,
     ):
         self._url = "https://rest.ensembl.org"
-        self._max_workers: int = max_workers
-        self._show_progress: bool = show_progress
-
         BaseModel.__init__(self, url=self._url)
         FastHTTP.__init__(
             self,
             cache=cache,
-            max_workers=self._max_workers,
+            workers=max_workers,
             max_requests_per_second=15,
-            show_progress=self._show_progress,
+            show_progress=show_progress,
         )
 
     @property
     def url(self) -> str:
         return self._url
+
+    def _get_species(self) -> List[Species]:
+        path = self._url + "/info/species"
+        response = self._get(path, headers={"Content-Type": "application/json"})
+        species: list[Species] = []
+        for item in response[0].json["species"]:
+            species.append(Species(**item))
+        return species
+
+    def _match_species(self, species: str) -> Optional[Species]:
+        """
+        This function will make sure the user enters a valid species name
+        It will do this by collecting all the species names from the Ensembl API
+        Then doing a fuzzy search on the species name
+        If the species name does not exactly match an alias from the API,
+        it will show a warning so the user knows they have not perfectly matched a species name
+
+        Parameters
+        ----------
+        species : str
+            The species to validate
+
+        Returns
+        -------
+        Species
+            The species object from the Ensembl API
+        """
+        species = species.lower()
+        species_list = self._get_species()
+        matches: list[list[str]] = []
+        for possible_matches in species_list:
+            result = 
+            matches.append(f)
+        print(matches)
+        return None
+
+
+def main():
+    e = Ensembl(max_workers=1, show_progress=True)
+    e._match_species("homo_sapien")
+
+
+if __name__ == "__main__":
+    main()
