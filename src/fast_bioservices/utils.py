0348b82c5e76cc9c097a65f54094f4893af3f5be
Josh Loecker <joshloecker@gmail.com>
2024-06-24 14:37:30 -0500

    feat: Refactor code structure and add base classes for bioservices

--- src/fast_bioservices/utils.py
+++
@@ -1,8 +0,0 @@
-def flatten(lists: list) -> list:
-    data = []
-    for item in lists:
-        if isinstance(item, list):
-            data.extend(flatten(item))
-        else:
-            data.append(item)
-    return data
