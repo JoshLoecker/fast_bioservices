56f63f2f3f6bea093f7876ee60f356bfafb7a13f
Josh Loecker <joshloecker@gmail.com>
2024-07-09 11:46:21 -0500

    Version bump

--- src/fast_bioservices/__init__.py
+++ src/fast_bioservices/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["BiGG", "BioDBNet", "Input", "Output", "Taxon"]
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __description__ = "A fast way to access and convert biological information"
 
 from fast_bioservices.bigg.bigg import BiGG
