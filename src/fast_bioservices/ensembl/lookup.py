88354138c03461777b8972da7214c54062e52ea9
Josh Loecker <joshloecker@icloud.com>
2024-11-11 09:16:23 -0600

    feat: initial framework for api access to Ensembl's '/lookup' endpoint
    Supports a subset of the '/lookup' endpoints

---
+++ src/fast_bioservices/ensembl/lookup.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import json
+from typing import Literal
+
+import pandas as pd
+from fast_http import _AsyncHTTPClient
+from pandas.core.computation.common import result_type_many
+
+
+class Lookup(_AsyncHTTPClient):
+    def __init__(self, cache: bool = True):
+        self._url: str = "https://rest.ensembl.org"
+        self._cache: bool = cache
+        self._max_requests_per_second: int = 12
+
+        super().__init__(cache=cache, max_requests_per_second=self._max_requests_per_second)
+
+    async def _process(self, *, url: str, as_type: Literal["ids", "symbols"], items: list[str]) -> list[dict]:
+        response = (
+            await self._post(
+                url,
+                data=json.dumps({as_type: items}),
+                headers={"Content-Type": "application/json", "Accept": "application/json"},
+            )
+        )[0]
+        as_json = json.loads(response)
+        return list(as_json.values())
+
+    async def lookup_by_ensembl(self, ensembl_ids: str | list[str]) -> list[dict]:
+        url = f"{self._url}/lookup/id"
+        ensembl_ids = [ensembl_ids] if isinstance(ensembl_ids, str) else ensembl_ids
+        return await self._process(url=url, as_type="ids", items=ensembl_ids)
+
+    async def by_symbol(self, symbols: str | list[str], species: str) -> list[dict]:
+        url = f"{self._url}/lookup/symbol/{species}"
+        symbols = [symbols] if isinstance(symbols, str) else symbols
+        return await self._process(url=url, as_type="symbols", items=symbols)
+
+
+async def _main():
+    e = Lookup(cache=False)
+    print(await e.lookup_by_ensembl(["ENSG00000157764", "ENSG00000157765"]))
+    print(await e.lookup_by_ensembl(["ENSG00000157764"]))
+    print(await e.by_symbol(["GNAS", "HBA1", "HBA2"], species="homo_sapiens"))
+    print(await e.by_symbol(["GNAS"], species="homo_sapiens"))
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(_main())
