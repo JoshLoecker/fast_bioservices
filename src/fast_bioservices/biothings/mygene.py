34b792bc6208583ff384639060e4bbc507c6bbda
Josh Loecker <joshloecker@icloud.com>
2024-11-15 16:26:38 -0600

    refactor: fix imports based on moving items into different files

--- src/fast_bioservices/biothings/mygene.py
+++ src/fast_bioservices/biothings/mygene.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 
+from fast_bioservices.common import Taxon, validate_taxon_id
 from fast_bioservices.fast_http import _AsyncHTTPClient
 
 
@@ -11,13 +12,15 @@ class MyGene(_AsyncHTTPClient):
         self._chunk_size: int = 1000
         super().__init__(cache=cache, max_requests_per_second=5)
 
-    async def gene(self, ids: str | list[str]) -> list[dict]:
+    async def gene(self, ids: str | list[str], taxon: int | str | Taxon) -> list[dict]:
         """
         :param ids: Entrez or Ensembl IDs
+        :param taxon: The NCBI Taxonomy ID to use
         :return:
         """
+        taxon_id = validate_taxon_id(taxon)
         ids = [ids] if isinstance(ids, str) else ids
-        url = f"{self._base_url}/gene"
+        url = f"{self._base_url}/gene?species={taxon_id}"
         chunks = [ids[i : i + self._chunk_size] for i in range(0, len(ids), self._chunk_size)]
         data = [json.dumps({"ids": chunk}) for chunk in chunks]
         responses = await self._post(url, data=data, headers={"Content-type": "application/json"})
@@ -35,7 +38,7 @@ class MyGene(_AsyncHTTPClient):
 
 async def _main():
     m = MyGene()
-    r = await m.gene(["ENSG00000170558", "ENSG00000153563"])
+    r = await m.gene(["ENSG00000170558", "ENSG00000153563"], taxon=Taxon.HOMO_SAPIENS)
     print(r[0].keys())
     print(r[0]["entrezgene"])
     print(r[0]["ensembl"]["gene"])
