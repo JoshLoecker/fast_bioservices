cabf58f921fcea15fe6d6ddf3bdcb326e139930e
Josh Loecker <joshloecker@icloud.com>
2024-11-15 13:48:17 -0600

    feat: add basics for sqlite database for faster cache access
    Database access is not yet implemented, only the framework to add it in the future

--- src/fast_bioservices/settings.py
+++ src/fast_bioservices/settings.py
@@ -5,6 +5,7 @@ import appdirs
 # Cache settings
 _root_cache_dir: Path = Path(appdirs.user_cache_dir("fast_bioservices"))
 cache_dir: Path = Path(_root_cache_dir, "fast_bioservices_cache")
+db_filepath: Path = Path(_root_cache_dir, "fast_bioservices.db")
 log_filepath: Path = Path(_root_cache_dir, "fast_bioservices.log")
 
 _root_cache_dir.mkdir(parents=True, exist_ok=True)
