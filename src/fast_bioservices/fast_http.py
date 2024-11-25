80b07f33b70d153b6bd5618a8b80a771fb0298bf
Josh Loecker <joshloecker@icloud.com>
2024-11-25 15:53:41 -0600

    refactor: remove unused imports and functions

--- src/fast_bioservices/fast_http.py
+++ src/fast_bioservices/fast_http.py
@@ -4,10 +4,8 @@ import asyncio
 import sys
 import time
 import urllib.parse
-from abc import ABC
 from typing import Literal, NamedTuple
 
-import anysqlite
 import hishel
 import httpcore
 import httpx
@@ -15,7 +13,7 @@ from hishel._utils import generate_key
 from httpx import Request, Response
 from loguru import logger
 
-from fast_bioservices.settings import cache_dir, db_filepath
+from fast_bioservices.settings import cache_dir
 
 
 class RequestSetup(NamedTuple):
@@ -44,11 +42,7 @@ def _key_generator(request: httpx.Request | httpcore.Request, body: bytes = b"")
     return f"{prefix}/{method}|{host}|{key}"
 
 
-async def _get_database_connection() -> anysqlite.Connection:
-    return await anysqlite.connect(db_filepath)
-
-
-def make_safe_url(urls: str | list[str]) -> str | list[str]:
+def _make_safe_url(urls: str | list[str]) -> str | list[str]:
     safe_chars = "&$+,/:;=?@#"
     if isinstance(urls, str):
         return urllib.parse.quote(urls, safe=safe_chars)
@@ -56,9 +50,7 @@ def make_safe_url(urls: str | list[str]) -> str | list[str]:
 
 
 class _AsyncRateLimitTransport(httpx.AsyncBaseTransport):
-    """
-    Implement rate limiting on httpx transports; ignores hishel transport cache
-    """
+    """Implement rate limiting on httpx transports."""
 
     def __init__(self, rate: int | float, period: int = 1):
         self.transport = httpx.AsyncHTTPTransport()
@@ -86,7 +78,7 @@ class _AsyncRateLimitTransport(httpx.AsyncBaseTransport):
         return await self.transport.handle_async_request(request)
 
 
-class _AsyncHTTPClient(ABC):
+class _AsyncHTTPClient:
     def __init__(self, *, cache: bool, max_requests_per_second) -> None:
         self._use_cache: bool = cache
         transport = _AsyncRateLimitTransport(rate=max_requests_per_second)
@@ -103,10 +95,13 @@ class _AsyncHTTPClient(ABC):
             self._transport = transport
         self._client: httpx.AsyncClient = httpx.AsyncClient(transport=self._transport, timeout=180)
 
-        self._semaphore = asyncio.Semaphore(value=10)
+        self._semaphore = asyncio.Semaphore(value=5)
         self.__current_requests: int = 0
         self.__total_requests: int = 0
         self.__log_per_step: int = 1
+        self.__working_time: float = 0.0
+        self.__chunk_time: float = 0.0
+        self.__padding: int = 0
 
     def update_rate_limit(self, value: int):
         self._transport.rate = value
@@ -115,8 +110,16 @@ class _AsyncHTTPClient(ABC):
         self.__current_requests += 1
 
         if self.__current_requests % self.__log_per_step == 0 or self.__current_requests == self.__total_requests:
+            current_time = time.time()
+            working_time = round(current_time - self.__working_time, 1)
+            chunk_time = round(current_time - self.__chunk_time, 1)
             ending = "with cache" if cached else "without cache"
-            logger.debug(f"Finished {self.__current_requests:>{len(str(self.__total_requests))}} of {self.__total_requests} ({ending})")
+            logger.debug(
+                f"Finished {self.__current_requests:>{self.__padding}} of {self.__total_requests} ({ending})"
+                f" - chunk took {chunk_time} seconds"
+                f" - running for {working_time} seconds"
+            )
+            self.__chunk_time = current_time
 
     async def __perform_action(self, func: Literal["get", "post"], url: str, /, log_on_complete: bool, **kwargs):
         try:
@@ -128,16 +131,16 @@ class _AsyncHTTPClient(ABC):
                     response = await self._client.post(url, **kwargs)
         except httpx.ReadTimeout as e:
             logger.critical(f"ReadTimeout: Timed out while receiving data from the host: {url}")
-            raise e
+            raise e from httpx.ReadTimeout
         except httpx.ConnectTimeout as e:
             logger.critical(f"ConnectTimeout: Timed out while connecting to the host: {url}")
-            raise e
+            raise e from httpx.ConnectTimeout
         except httpx.ConnectError as e:
             logger.critical(f"ConnectError: Failed to establish a connection: {url}")
-            raise e
+            raise e from httpx.ConnectError
 
         if log_on_complete:
-            if "from_cache" in response.extensions and response.extensions["from_cache"]:
+            if response.extensions.get("from_cache"):
                 self._log_callback(cached=True)
             else:
                 self._log_callback(cached=False)
@@ -145,11 +148,16 @@ class _AsyncHTTPClient(ABC):
 
     def _setup_action(self) -> None:
         # Show update every 10% with a minimum of every 1000
+        self.__padding = len(str(self.__total_requests))
+
+        self.__working_time = time.time()
+        self.__chunk_time = time.time()
+
         self.__log_per_step = int(self.__total_requests * 0.1)
         if self.__log_per_step < 1:
             self.__log_per_step = 1
         self.__log_per_step = min(1000, self.__log_per_step)
-        logger.debug(f"Will show progress every {self.__log_per_step} steps")
+        logger.debug(f"Will show progress every {self.__log_per_step} steps ({self.__total_requests} total steps)")
 
     async def _get(
         self,
@@ -161,7 +169,7 @@ class _AsyncHTTPClient(ABC):
     ) -> list[bytes]:
         # Make urls safe
         # Safe characters from https://stackoverflow.com/questions/695438
-        urls: list[str] = [make_safe_url(urls)] if isinstance(urls, str) else make_safe_url(urls)
+        urls: list[str] = [_make_safe_url(urls)] if isinstance(urls, str) else _make_safe_url(urls)
         self.__current_requests = 0
         self.__total_requests = len(urls)
         headers = headers or {}
@@ -177,13 +185,14 @@ class _AsyncHTTPClient(ABC):
     async def _post(
         self,
         url: str,
+        *,
         data: str | list[str],
         headers: dict | None = None,
         temp_disable_cache: bool = False,
         log_on_complete: bool = True,
         extensions: dict | None = None,
     ) -> list[bytes]:
-        url: str = make_safe_url(url)
+        url: str = _make_safe_url(url)
         self.__current_requests = 0
         self.__total_requests = 1 if isinstance(data, str) else len(data)
         headers = headers or {}
