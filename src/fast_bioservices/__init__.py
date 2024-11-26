31b4b209d368b1a8cbebb9cf1371b9a130d4410d
Josh Loecker <joshloecker@icloud.com>
2024-11-26 10:51:43 -0600

    fix: allow importing pipeline

--- src/fast_bioservices/__init__.py
+++ src/fast_bioservices/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["BioDBNet", "Input", "Output", "Taxon"]
+__all__ = ["BioDBNet", "Input", "Output", "Taxon", "pipeline"]
 __version__ = "0.3.5"
 __description__ = "A fast way to access and convert biological information"
 
@@ -6,3 +6,4 @@ __description__ = "A fast way to access and convert biological information"
 from fast_bioservices.biodbnet.biodbnet import BioDBNet
 from fast_bioservices.biodbnet.nodes import Input, Output
 from fast_bioservices.common import Taxon
+from fast_bioservices import pipeline
