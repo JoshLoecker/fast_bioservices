68f5801ac4eb1b8b0f7fa461f91f7590a9308755
Josh Loecker <joshloecker@icloud.com>
2024-12-04 11:42:13 -0600

    refactor: remove unused ensembl tests

--- tests/test_biodbnet.py
+++ tests/test_biodbnet.py
@@ -1,167 +1,213 @@
+from __future__ import annotations
+
+import asyncio
+
 import pandas as pd
 import pytest
 
 from fast_bioservices import BioDBNet, Input, Output, Taxon
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def biodbnet_no_cache() -> BioDBNet:
-    return BioDBNet(max_workers=1, cache=False)
+    return BioDBNet(cache=False)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def biodbnet_cache() -> BioDBNet:
-    return BioDBNet(max_workers=1, cache=True)
+    return BioDBNet(cache=True)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def gene_ids() -> list[str]:
     return ["4318", "1376", "2576", "10089"]
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def gene_symbols() -> list[str]:
     return ["MMP9", "CPT2", "GAGE4", "KCNK7"]
 
 
-def test_dbOrg(biodbnet_no_cache, biodbnet_cache) -> None:
-    result_nocache = biodbnet_no_cache.db_org(
-        input_db=Input.ENSEMBL_GENE_ID,
-        output_db=Output.GENE_ID,
-        taxon=Taxon.HOMO_SAPIENS,
-    )
-    result_cache = biodbnet_no_cache.db_org(
-        input_db=Input.ENSEMBL_GENE_ID,
-        output_db=Output.GENE_ID,
-        taxon=Taxon.HOMO_SAPIENS,
-    )
-
-    assert isinstance(result_nocache, pd.DataFrame)
-    assert isinstance(result_cache, pd.DataFrame)
-    assert len(result_nocache) > 0
-    assert len(result_cache) > 0
-
-
-def test_getDirectOutputsForInput(biodbnet_no_cache, biodbnet_cache):
-    result_nocache = biodbnet_no_cache.get_direct_outputs_for_input(Input.GENE_ID)
-    result_cache = biodbnet_cache.get_direct_outputs_for_input(Input.GENE_ID)
-
-    assert isinstance(result_nocache, list)
-    assert isinstance(result_cache, list)
-    assert len(result_nocache) > 0
-    assert len(result_cache) > 0
-
-
-def test_getInputs(biodbnet_no_cache, biodbnet_cache):
-    result_nocache = biodbnet_no_cache.get_inputs()
-    result_cache = biodbnet_cache.get_inputs()
-    assert isinstance(result_nocache, list)
-    assert isinstance(result_cache, list)
-    assert len(result_nocache) > 0
-    assert len(result_cache) > 0
-
-
-def test_getOutputsForInput(biodbnet_no_cache, biodbnet_cache):
-    result_nocache = biodbnet_no_cache.get_outputs_for_input(Input.GENE_SYMBOL)
-    result_cache = biodbnet_cache.get_outputs_for_input(Input.GENE_SYMBOL)
-    assert isinstance(result_nocache, list)
-    assert isinstance(result_cache, list)
-    assert len(result_nocache) > 0
-    assert len(result_cache) > 0
-
-
-def test_db2db(biodbnet_no_cache, biodbnet_cache, gene_ids, gene_symbols):
-    result_nocache = biodbnet_no_cache._db2db(
-        values=gene_ids,
-        input_db=Input.GENE_ID,
-        output_db=Output.GENE_SYMBOL,
-        taxon=Taxon.HOMO_SAPIENS,
-    )
-    result_cache = biodbnet_cache._db2db(
-        values=gene_ids,
-        input_db=Input.GENE_ID,
-        output_db=Output.GENE_SYMBOL,
-        taxon=Taxon.HOMO_SAPIENS,
+@pytest.mark.asyncio
+async def test_db_org(biodbnet_no_cache, biodbnet_cache) -> None:
+    (no_cache, with_cache) = await asyncio.gather(
+        *[
+            biodbnet_no_cache.db_org(
+                input_db=Input.ENSEMBL_GENE_ID,
+                output_db=Output.GENE_ID,
+                taxon=Taxon.HOMO_SAPIENS,
+            ),
+            biodbnet_no_cache.db_org(
+                input_db=Input.ENSEMBL_GENE_ID,
+                output_db=Output.GENE_ID,
+                taxon=Taxon.HOMO_SAPIENS,
+            ),
+        ]
     )
 
-    assert "Gene ID" in result_nocache.columns
-    assert "Gene ID" in result_cache.columns
-    assert "Gene Symbol" in result_nocache.columns
-    assert "Gene Symbol" in result_cache.columns
+    assert isinstance(no_cache, pd.DataFrame)
+    assert isinstance(with_cache, pd.DataFrame)
+    assert len(no_cache) > 0
+    assert len(with_cache) > 0
+
+
+@pytest.mark.asyncio
+async def test_get_direct_outputs_for_input(biodbnet_no_cache, biodbnet_cache):
+    (no_cache, with_cache) = await asyncio.gather(
+        *[
+            biodbnet_no_cache.get_direct_outputs_for_input(Input.GENE_ID),
+            biodbnet_cache.get_direct_outputs_for_input(Input.GENE_ID),
+        ]
+    )
+
+    assert isinstance(no_cache, list)
+    assert isinstance(with_cache, list)
+    assert len(no_cache) > 0
+    assert len(with_cache) > 0
+
+
+@pytest.mark.asyncio
+async def test_get_inputs(biodbnet_no_cache, biodbnet_cache):
+    (no_cache, with_cache) = await asyncio.gather(
+        *[
+            biodbnet_no_cache.get_inputs(),
+            biodbnet_cache.get_inputs(),
+        ]
+    )
+
+    assert isinstance(no_cache, list)
+    assert isinstance(with_cache, list)
+    assert len(no_cache) > 0
+    assert len(with_cache) > 0
+
+
+@pytest.mark.asyncio
+async def test_get_outputs_for_input(biodbnet_no_cache, biodbnet_cache):
+    (no_cache, with_cache) = await asyncio.gather(
+        *[
+            biodbnet_no_cache.get_outputs_for_input(Input.GENE_SYMBOL),
+            biodbnet_cache.get_outputs_for_input(Input.GENE_SYMBOL),
+        ]
+    )
+
+    assert isinstance(no_cache, list)
+    assert isinstance(with_cache, list)
+    assert len(no_cache) > 0
+    assert len(with_cache) > 0
+
+
+@pytest.mark.asyncio
+async def test_db2db(biodbnet_no_cache, biodbnet_cache, gene_ids, gene_symbols):
+    (no_cache, with_cache) = await asyncio.gather(
+        *[
+            biodbnet_no_cache._db2db(
+                values=gene_ids,
+                input_db=Input.GENE_ID,
+                output_db=Output.GENE_SYMBOL,
+                taxon=Taxon.HOMO_SAPIENS,
+            ),
+            biodbnet_cache._db2db(
+                values=gene_ids,
+                input_db=Input.GENE_ID,
+                output_db=Output.GENE_SYMBOL,
+                taxon=Taxon.HOMO_SAPIENS,
+            ),
+        ]
+    )
+
+    assert "Gene ID" in no_cache.columns
+    assert "Gene ID" in with_cache.columns
+    assert "Gene Symbol" in no_cache.columns
+    assert "Gene Symbol" in with_cache.columns
 
     for id_, symbol in zip(gene_ids, gene_symbols):
-        assert id_ in result_nocache["Gene ID"].values
-        assert id_ in result_cache["Gene ID"].values
-        assert symbol in result_nocache["Gene Symbol"].values
-        assert symbol in result_cache["Gene Symbol"].values
+        assert id_ in no_cache["Gene ID"].values
+        assert id_ in with_cache["Gene ID"].values
+        assert symbol in no_cache["Gene Symbol"].values
+        assert symbol in with_cache["Gene Symbol"].values
 
 
-def test_dbWalk(biodbnet_no_cache, biodbnet_cache):
-    result_nocache = biodbnet_no_cache.db_walk(
-        values=["4318", "1376", "2576", "10089"],
-        db_path=[Input.GENE_ID, Input.GENE_SYMBOL],
-        taxon=Taxon.HOMO_SAPIENS,
+@pytest.mark.asyncio
+async def test_db_walk(biodbnet_no_cache, biodbnet_cache):
+    no_cache, with_cache = await asyncio.gather(
+        *[
+            biodbnet_no_cache.db_walk(
+                values=["4318", "1376", "2576", "10089"],
+                db_path=[Input.GENE_ID, Input.GENE_SYMBOL],
+                taxon=Taxon.HOMO_SAPIENS,
+            ),
+            biodbnet_cache.db_walk(
+                values=["4318", "1376", "2576", "10089"],
+                db_path=[Input.GENE_ID, Input.GENE_SYMBOL],
+                taxon=Taxon.HOMO_SAPIENS,
+            ),
+        ]
     )
-    result_cache = biodbnet_cache.db_walk(
-        values=["4318", "1376", "2576", "10089"],
-        db_path=[Input.GENE_ID, Input.GENE_SYMBOL],
-        taxon=Taxon.HOMO_SAPIENS,
-    )
 
-    assert len(result_nocache) == len(result_cache) == 4
+    assert len(no_cache) == len(with_cache) == 4
 
 
 @pytest.mark.skip(reason="dbReport not yet implemented")
-def test_dbReport(biodbnet_no_cache):
-    biodbnet_no_cache.db_report(values=["4318"], input_db=Input.GENE_ID, taxon=Taxon.HOMO_SAPIENS)
+async def test_db_report(biodbnet_no_cache):
+    await biodbnet_no_cache.db_report(values=["4318"], input_db=Input.GENE_ID, taxon=Taxon.HOMO_SAPIENS)
 
 
-def test_dbFind(biodbnet_no_cache, biodbnet_cache, gene_ids, gene_symbols):
-    result_nocache = biodbnet_no_cache.db_find(values=gene_ids, output_db=Output.GENE_SYNONYMS, taxon=Taxon.HOMO_SAPIENS)
-    result_cache = biodbnet_cache.db_find(values=gene_ids, output_db=Output.GENE_SYNONYMS, taxon=Taxon.HOMO_SAPIENS)
+@pytest.mark.asyncio
+async def test_db_find(biodbnet_no_cache, biodbnet_cache, gene_ids, gene_symbols):
+    no_cache, with_cache = await asyncio.gather(
+        *[
+            biodbnet_no_cache.db_find(values=gene_ids, output_db=Output.GENE_SYNONYMS, taxon=Taxon.HOMO_SAPIENS),
+            biodbnet_cache.db_find(values=gene_ids, output_db=Output.GENE_SYNONYMS, taxon=Taxon.HOMO_SAPIENS),
+        ]
+    )
 
-    assert len(result_nocache) == len(result_cache) == 4
+    assert len(no_cache) == len(with_cache) == 4
     for id_, symbol in zip(gene_ids, gene_symbols):
-        assert id_ in result_nocache["InputValue"].values
-        assert id_ in result_cache["InputValue"].values
-        assert symbol in result_nocache["Gene Symbol"].values
-        assert symbol in result_cache["Gene Symbol"].values
+        assert id_ in no_cache["InputValue"].values
+        assert id_ in with_cache["InputValue"].values
+        assert symbol in no_cache["Gene Symbol"].values
+        assert symbol in with_cache["Gene Symbol"].values
 
 
-def test_dbOrtho(biodbnet_no_cache, biodbnet_cache, gene_ids):
-    result_nocache = biodbnet_no_cache.db_ortho(
-        values=gene_ids,
-        input_db=Input.GENE_ID,
-        output_db=Output.GENE_SYMBOL,
-        input_taxon=Taxon.HOMO_SAPIENS,
-        output_taxon=Taxon.MUS_MUSCULUS,
+@pytest.mark.asyncio
+async def test_db_ortho(biodbnet_no_cache, biodbnet_cache, gene_ids):
+    no_cache, with_cache = await asyncio.gather(
+        *[
+            biodbnet_no_cache.db_ortho(
+                values=gene_ids,
+                input_db=Input.GENE_ID,
+                output_db=Output.GENE_SYMBOL,
+                input_taxon=Taxon.HOMO_SAPIENS,
+                output_taxon=Taxon.MUS_MUSCULUS,
+            ),
+            biodbnet_cache.db_ortho(
+                values=gene_ids,
+                input_db=Input.GENE_ID,
+                output_db=Output.GENE_SYMBOL,
+                input_taxon=Taxon.HOMO_SAPIENS,
+                output_taxon=Taxon.MUS_MUSCULUS,
+            ),
+        ]
     )
-    result_cache = biodbnet_cache.db_ortho(
-        values=gene_ids,
-        input_db=Input.GENE_ID,
-        output_db=Output.GENE_SYMBOL,
-        input_taxon=Taxon.HOMO_SAPIENS,
-        output_taxon=Taxon.MUS_MUSCULUS,
-    )
 
-    assert len(result_nocache) == len(result_cache) == 4
+    assert len(no_cache) == len(with_cache) == 4
 
     # symbols are from Mus Musculus, not checking those
     for id_ in zip(gene_ids):
-        assert id_ in result_nocache["Gene ID"].values
-        assert id_ in result_cache["Gene ID"].values
+        assert id_ in no_cache["Gene ID"].values
+        assert id_ in with_cache["Gene ID"].values
 
 
 @pytest.mark.skip(reason="dbAnnot tests not yet written")
-def test_dbAnnot(biodbnet_no_cache):
+def test_db_annot(biodbnet_no_cache):
     pass
 
 
 @pytest.mark.skip(reason="getAllPathways tests not yet written")
-def test_getAllPathways(biodbnet_no_cache):
+def test_get_all_pathways(biodbnet_no_cache):
     pass
 
 
 @pytest.mark.skip(reason="getPathwayFromDatabase tests not yet written")
-def test_getPathwayFromDatabase(biodbnet_no_cache):
+def test_get_pathway_from_database(biodbnet_no_cache):
     pass
